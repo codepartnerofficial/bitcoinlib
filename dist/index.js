1d5678fe4343b645d21039d270455fd345efec48
Pralay Mandal <p14041999@gmail.com>
2025-07-17 22:58:18 +0530

    added fix

--- dist/index.js
+++ dist/index.js
@@ -239,7 +239,7 @@ class Bitcoin {
                 console.log("Broadcast Transaction Failed");
                 // throw error
                 if (this.networkName == "mainnet") {
-                    return 100;
+                    return 20;
                 }
                 else {
                     return 1.2;
@@ -255,47 +255,3 @@ class Bitcoin {
     }
 }
 exports.Bitcoin = Bitcoin;
-// const btc = new Bitcoin();
-// const addressObject = {
-//     address: 'mnUVDize2xtFDvrHrbn3j2mEVMo5YxVABQ',
-//     bchAddress: 'bchtest:qpx9zu4zd262vt2rvkvc5w2dmr2hfe0m9scyxv5ued',
-//     privateKey: 'L2T7UtdABsezrqjv3razhvfiDNBXgaezu9RRv3C6sYULAgWWcW5g'
-// }
-// https://blockexplorer.one/bitcoin/testnet/tx/85a5f23220cc7c19eb86d0c61342611f5fcfc2dbd96f5cce09ea42c5e08658c1
-// https://blockexplorer.one/bitcoin/testnet/tx/9e171929820f74b4ea62543c278161003dc2475a144afa36c6caf8e3fb8981c9
-// https://blockexplorer.one/bitcoin/testnet/tx/1ce16435561c7d3b9aa3eb99165af655528315df2e1c4c8aeb6824e8b03e8b5f
-const vout = {
-    amount: 0.00117331,
-    vout: 0,
-    txid: "1ce16435561c7d3b9aa3eb99165af655528315df2e1c4c8aeb6824e8b03e8b5f"
-};
-const addressObject = {
-    address: 'tb1qz35fguvz79peeyjudhg27r7cj5jle3hq49jf7u',
-    bchAddress: 'bchtest:qq2x39r3stc588yjt3kaptc0mz2jtlxxuqeqpdzyrg',
-    privateKey: 'KxKKjBgDnUpYpzFvZHjSNpjZAY7Pqi1k4iMNpE15y1EdFvtTVDBV'
-};
-const toAddress = {
-    address: 'tb1qz35fguvz79peeyjudhg27r7cj5jle3hq49jf7u',
-    bchAddress: 'bchtest:qq2x39r3stc588yjt3kaptc0mz2jtlxxuqeqpdzyrg',
-    privateKey: 'KxKKjBgDnUpYpzFvZHjSNpjZAY7Pqi1k4iMNpE15y1EdFvtTVDBV'
-};
-const rawTx = "020000000001011ddcc4d656ca2e11cc2c0cdc987018f7396c9d1dcc12f866800da981c0aec5950100000000fdffffff028dccd046000000001976a914795dfe1c25c1ae9965f45d66383db2bed567ac4c88ac98d50100000000001976a9144c5172a26ab4a62d4365998a394dd8d574e5fb2c88ac01409d3c376c2ef04d134b215e4acd43f383920cc7667c06aceeba7eacfe42c14bb99ae64e572007f5ddbfe568c97bb2351dd97fcb4697a6c98144cd9b9aa722551d95d34500";
-// 0.00119868 - 0.00003440
-// ===========================
-// Generate Address
-// ===========================
-// console.log("Address: ",Bitcoin.generateAddress('testnet','segwit'))
-// ===========================
-// Send Transaction
-// ===========================
-const btc = new Bitcoin("https://bitcoin-testnet.g.alchemy.com/v2/D7iVzbC_LzMwoRzi58DhtYnv_pqG2EEq", "testnet");
-// SegWit [p2wpkh]
-btc.sendBitcoinSegWit(vout, [{ address: toAddress.address, sendRemaining: true }], addressObject.privateKey)
-    // Public Key Hash
-    // const tx = btc.sendBitcoin(rawTx, vout, [{ address: toAddress.address, amount: 0.0012 }], addressObject.privateKey)
-    .then(tx => {
-    console.log("Raw Transaction:", tx);
-    btc.broadcastTransaction(tx)
-        .then(response => { console.log("Broadcast Response", response); })
-        .catch(err => console.log(err));
-}).catch(console.error);
